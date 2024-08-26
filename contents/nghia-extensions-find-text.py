a9755b5c687373dd74a1dd23c2bc3fb2a0fd7bd8
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-08-26 15:36:04 +0700

    update nghia-extensions-find-text.py and output.txt

--- contents/nghia-extensions-find-text.py
+++ contents/nghia-extensions-find-text.py
@@ -2,59 +2,44 @@ import os
 import glob
 
 
-
-def find(directory, keywords, extensions,   output_file=None, ignore_folders=None): 
+def find(directory, keywords, extensions, output_file=None, ignore_folders=None):
     files = []
     for extension in extensions:
-        files +=    glob.glob(os.path.join(directory, f'**/*{extension}'), recursive=True)
-    print(files)
+        files += glob.glob(os.path.join(directory, f'**/*{extension}'), recursive=True)
+
+    if ignore_folders:
+        files = [file for file in files if not any(ig in file for ig in ignore_folders)]
+
+    with open(output_file, "a", encoding="utf-8") as out_file:
+        for file_path in files:
+            try:
+                with open(file_path, "r", encoding="utf-8") as f:
+                    content = f.read()
+                    if any(keyword in content for keyword in keywords):
+                        out_file.write(f"{file_path}\n")
+            except Exception as e:
+                print(f"Could not read file {file_path}: {e}")
 
 
-
-
-
-    
-# 
- 
-    #         if os.path.basename(item_path) not in ignore_folders:
-
-
-
-    with open(output_file, "a", encoding="utf-8") as file:
-        file.write( path+ "\n")
-
-     
-
-
-
-
-
-
-
-
+if __name__ == "__main__":
+    #! INPUT
+    folder_path = r"C:\Users\vvn20206205\Downloads\Nghia\Git\whynotnghiavu\VideoVN"
+    keywords = [
+        # "#",
+        #  "NOTE",
+        #  "print",
+        "replace",
+    ]
+    extensions = [".py"]
+    output_file = "output.txt"
+    ignore_folders = [
+        ".git",
+        "node_modules",
+        "__pycache__"
+    ]
 #! INPUT
-keywords = "#"
-keywords = "NOTE"
-keywords = "print"
-keywords = "replace"
 
-
-extensions = [
-    ".py",
-]
-
-
-folder_path = r"C:\Users\vvn20206205\Downloads\Nghia\Git\whynotnghiavu\VideoVN" 
-
-output_file = "output.txt"
-ignore_folders = [
-    ".git", 
-    "node_modules", 
-    "__pycache__"
-]
-#! INPUT
-with open(output_file, 'w', encoding="utf-8") as file:
-    file.write(f"PATH: {folder_path}\nKeywords: {keywords}\n\n\n")
-
-find(folder_path,  keywords,   extensions  ,  output_file=output_file, ignore_folders=ignore_folders)
-print(f"Kết quả đã được ghi vào tập tin {output_file}")
+    with open(output_file, 'w', encoding="utf-8") as file:
+        file.write(f"PATH: {folder_path}\nKeywords: {keywords}\n\n\n")
+    find(folder_path, keywords, extensions, output_file=output_file, ignore_folders=ignore_folders)
+    print(f"Kết quả đã được ghi vào tập tin {output_file}")
