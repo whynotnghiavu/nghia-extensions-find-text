9c2ee66043d2ab675096dabc5daf1350616415c2
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-08-26 15:29:16 +0700

    update nghia-extensions-find-text.py

--- contents/nghia-extensions-find-text.py
+++ contents/nghia-extensions-find-text.py
@@ -47,7 +47,11 @@ extensions = [
 folder_path = r"C:\Users\vvn20206205\Downloads\Nghia\Git\whynotnghiavu\VideoVN" 
 
 output_file = "output.txt"
-ignore_folders = [".git", "node_modules", "__pycache__"]
+ignore_folders = [
+    ".git", 
+    "node_modules", 
+    "__pycache__"
+]
 #! INPUT
 with open(output_file, 'w', encoding="utf-8") as file:
     file.write(f"PATH: {folder_path}\nKeywords: {keywords}\n\n\n")
