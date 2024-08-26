0b8a1d8f3e5fea6ec7f85f5cb91c62be857bcc80
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-08-26 12:05:25 +0700

    update nghia-extensions-find-text.py

--- contents/nghia-extensions-find-text.py
+++ contents/nghia-extensions-find-text.py
@@ -1,7 +1,28 @@
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 #! INPUT
-folder_path = r"C:\Users\VuVanNghia20206205\Desktop\flask-hello-world" 
+text = "#"
+text = "NOTE"
+text = "print"
+text = "replace"
+
+folder_path = r"C:\Users\vvn20206205\Downloads\Nghia\Git\whynotnghiavu\VideoVN" 
 
 output_file = "output.txt"
 ignore_folders = [".git", "node_modules", "__pycache__"]
@@ -9,5 +30,5 @@ ignore_folders = [".git", "node_modules", "__pycache__"]
 with open(output_file, 'w', encoding="utf-8") as file:
     file.write(f"PATH: {folder_path}\n\n\n")
 
-# display(folder_path, output_file=output_file, ignore_folders=ignore_folders)
+find(folder_path, output_file=output_file, ignore_folders=ignore_folders)
 print(f"Kết quả đã được ghi vào tập tin {output_file}")
