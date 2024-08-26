60a122aa42222cda242b402a3d1af64a291c27c9
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-08-26 12:08:21 +0700

    update nghia-extensions-find-text.py and output.txt

--- contents/nghia-extensions-find-text.py
+++ contents/nghia-extensions-find-text.py
@@ -13,14 +13,11 @@
 
 
 
-
-
-
 #! INPUT
-text = "#"
-text = "NOTE"
-text = "print"
-text = "replace"
+keywords = "#"
+keywords = "NOTE"
+keywords = "print"
+keywords = "replace"
 
 folder_path = r"C:\Users\vvn20206205\Downloads\Nghia\Git\whynotnghiavu\VideoVN" 
 
@@ -28,7 +25,7 @@ output_file = "output.txt"
 ignore_folders = [".git", "node_modules", "__pycache__"]
 #! INPUT
 with open(output_file, 'w', encoding="utf-8") as file:
-    file.write(f"PATH: {folder_path}\n\n\n")
+    file.write(f"PATH: {folder_path}\nKeywords: {keywords}\n\n\n")
 
-find(folder_path, output_file=output_file, ignore_folders=ignore_folders)
+# find(folder_path, output_file=output_file, ignore_folders=ignore_folders)
 print(f"Kết quả đã được ghi vào tập tin {output_file}")
