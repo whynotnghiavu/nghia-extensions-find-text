a19c74d1ed8390a751b4e45582af117458f73d6b
whynotnghiavu <176358444+whynotnghiavu@users.noreply.github.com>
2024-08-26 12:17:50 +0700

    update nghia-extensions-find-text.py and output.txt

--- contents/nghia-extensions-find-text.py
+++ contents/nghia-extensions-find-text.py
@@ -1,10 +1,24 @@
+import os
+import glob
 
 
 
+def find(directory, keywords, extensions,   output_file=None, ignore_folders=None): 
+
+    files = []
+    for extension in extensions:
+        files +=    glob.glob(os.path.join(directory, f'**/*{extension}'), recursive=True)
+    print(files)
+
+ 
+    #         if os.path.basename(item_path) not in ignore_folders:
 
 
 
+    with open(output_file, "a", encoding="utf-8") as file:
+        file.write( "nghia" + "\n")
 
+     
 
 
 
@@ -19,6 +33,12 @@ keywords = "NOTE"
 keywords = "print"
 keywords = "replace"
 
+
+extensions = [
+    ".py",
+]
+
+
 folder_path = r"C:\Users\vvn20206205\Downloads\Nghia\Git\whynotnghiavu\VideoVN" 
 
 output_file = "output.txt"
@@ -27,5 +47,5 @@ ignore_folders = [".git", "node_modules", "__pycache__"]
 with open(output_file, 'w', encoding="utf-8") as file:
     file.write(f"PATH: {folder_path}\nKeywords: {keywords}\n\n\n")
 
-# find(folder_path, output_file=output_file, ignore_folders=ignore_folders)
+find(folder_path,  keywords,   extensions  ,  output_file=output_file, ignore_folders=ignore_folders)
 print(f"Kết quả đã được ghi vào tập tin {output_file}")
